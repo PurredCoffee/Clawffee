f14b8c1febc0051eea027276677f9f0e8425262c
PurredCoffee <owo@foxgirl.codes>
2025-07-09 01:36:28 +0200

    add twitch config

--- plugins/builtin/helpers.js
+++ plugins/builtin/helpers.js
@@ -2,3 +2,12 @@ const files = require('./files');
 const server = require('./server');
 const subscribable = require('./subscribable');
 const twitch = require('./twitch');
+const obs = require('./obs');
+
+module.exports = {
+    files,
+    server,
+    subscribable,
+    twitch,
+    obs
+}
