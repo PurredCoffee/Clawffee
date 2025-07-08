c57d5a1604d51e5e8521267a75fc0ed894a090e4
PurredCoffee <owo@foxgirl.codes>
2025-07-09 01:36:29 +0200

    improve module loading

--- plugins/builtin/internal/pluginReg.js
+++ plugins/builtin/internal/pluginReg.js
@@ -1,7 +1,14 @@
 function reloadPlugin(filename) {
-    setTimeout(() => require('../../../internal/commandHotReloader').reloadDependency(filename), 1);
+    setTimeout(() => {
+        require('../../../internal/pluginLoader').unavailablePlugins[filename] = false;
+        require('../../../internal/commandHotReloader').reloadDependency(filename)
+    }, 1);
+}
+function blockPlugin(filename) {
+    setTimeout(() => require('../../../internal/pluginLoader').unavailablePlugins[filename] = true, 1);
 }
 
 module.exports = {
-    reloadPlugin
+    reloadPlugin,
+    blockPlugin
 }
