a052c37a46fd824d617b8cce741ffa7162bbb774
PurredCoffee <owo@foxgirl.codes>
2025-07-12 17:24:02 +0200

    fix plugin import

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { requirePluginsRecursively } = require("./internal/pluginLoader")
 const { loadedmodules } = require("./internal/commandFSHooks");
 const { loadModule } = require("./internal/commandHotReloader");
 const path = require('path');
-const pluginsDir = path.resolve(__dirname, './plugins');
+const fs = require('fs');
+
+const pluginsDir = fs.realpathSync('./plugins');
 
 requirePluginsRecursively(pluginsDir + '/builtin');
 requirePluginsRecursively(pluginsDir);
