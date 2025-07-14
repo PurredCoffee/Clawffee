cdacdc6b611444405deafb21d110309e3ddaf17f
PurredCoffee <owo@foxgirl.codes>
2025-07-14 05:00:30 +0200

    add basic webview

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const dependencyInstaller = require('./internal/dependencyInstaller');
 
+const worker = new Worker('./test.js');
+
+worker.onmessage = (ev) => {
+    if(ev.data === 'exit') {
+        process.exit();
+    }
+}
+
 dependencyInstaller.downloadDependencies('./plugins').then(() => {
-    console.log("\n Clawffee Version 0.2.0");
+    console.log("\n Clawffee Version 0.2.1");
     console.log("-".repeat(48));
     const { requirePluginsRecursively } = require("./internal/pluginLoader")
     const { loadedmodules } = require("./internal/commandFSHooks");
