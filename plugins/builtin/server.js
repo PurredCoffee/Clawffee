cd281cd1c06415e4939ea1f3c387c219e30e413f
PurredCoffee <owo@foxgirl.codes>
2025-08-23 21:32:24 +0200

    fix opening websites for windows again

--- plugins/builtin/server.js
+++ plugins/builtin/server.js
@@ -1,6 +1,10 @@
 function openURL(url) {
-    var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'explorer': 'xdg-open');
-    require('child_process').execFileSync(start, [url]);
+    try {
+        var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'explorer': 'xdg-open');
+        require('child_process').execFileSync(start, [url]);
+    } catch(e) {
+        // ignore the error since explorer always returns 1
+    }
 }
 
 const { server: { setFunction, sharedServerData } } = require('../internal/internal');
